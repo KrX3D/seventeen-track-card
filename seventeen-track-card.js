c53a0443217b9f8fe652988af6da4c5e4521346e
KrX3D <KrX3D@users.noreply.github.com>
2022-05-15 15:02:34 +0100

    Update seventeen-track-card.js

--- seventeen-track-card.js
+++ seventeen-track-card.js
@@ -24,7 +24,7 @@ class SeventeenTrackCard extends HTMLElement {
       style.textContent = `
         table {
           width: 100%;
-          padding: 0 32px 32px 32px;
+          padding: 0 32px 20px 46px;
         }
         thead th {
           text-align: left;
